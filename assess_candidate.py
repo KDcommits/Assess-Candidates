f8c4157077fa955a2537cbc325a448eadc6e5ea6
Krishnendu <kd.datascience@gmail.com>
2023-07-31 17:23:22 +0530

    Refactoring code with MCQ answers

--- assess_candidate.py
+++ assess_candidate.py
@@ -7,36 +7,72 @@ load_dotenv()
 collection  = pymongo.MongoClient(os.getenv("MONGO_URI") )['Resume']['Resume']
 candidate_qna = pymongo.MongoClient(os.getenv("MONGO_URI") )['Resume']['Candidate_QnA']
 
+class AssessCandidate:
+    def __init__(self,candidate_email_id):
+        self.candidate_email_id = candidate_email_id
+        self.model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
 
-def calculate_semantic_similarity(model, sentence1, sentence2):
-    '''
-        Calculate Similartity score between GPT answer and candidate answer
-    '''
-    embeddings = model.encode([sentence1, sentence2], convert_to_tensor=True)
-    cosine_sim = util.pytorch_cos_sim(embeddings[0], embeddings[1])
-    return cosine_sim.item()
+    def _calculate_semantic_similarity(self,sentence1, sentence2):
+        '''
+            Calculate Similartity score between GPT answer and candidate answer
+        '''
+        embeddings = self.model.encode([sentence1, sentence2], convert_to_tensor=True)
+        cosine_sim = util.pytorch_cos_sim(embeddings[0], embeddings[1])
+        return cosine_sim.item()
     
-def updateCandidateDescriptiveAnswers(candidate_email_id, candidate_answers) -> float:
-    '''
-    Update answer of each question from the candidate 
-    '''
-    model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
-    filter_={"email":candidate_email_id}
-    gpt_answers = candidate_qna.find(filter_, {'descriptive_qna'}).next()['descriptive_qna']
-    scores = []
-    for i in range(len(candidate_answers)):
-        if candidate_answers[i]!="":
-            candidate_answer = candidate_answers[i]
-            gpt_answer  =  gpt_answers[i]['answer']
-            similarity_score = calculate_semantic_similarity(model, candidate_answer, gpt_answer)
-            scores.append(similarity_score)
-            candidate_qna.update_one({"email": candidate_email_id},
-                            {"$set": {f"descriptive_qna.{i}.candidate_answer": candidate_answer,
-                                      f"descriptive_qna.{i}.score": similarity_score}})
-        else:
-            scores.append(0)
-            candidate_qna.update_one({"email": candidate_email_id},
-                            {"$set": {f"descriptive_qna.{i}.candidate_answer": "No Answer Written",
-                                      f"descriptive_qna.{i}.score": 0}})
-            
-    return np.mean(scores)
+    def updateCandidateDescriptiveAnswers(self,descriptive_answers:list) -> float:
+        '''
+        Update answer of each descriptive question from the candidate and score them
+        '''
+        filter_={"email":self.candidate_email_id}
+        gpt_answers = candidate_qna.find(filter_, {'resume_descriptive_qna'}).next()['resume_descriptive_qna']
+        scores = []
+        for i in range(len(descriptive_answers)):
+            if descriptive_answers[i]!="":
+                candidate_descriptive_answer = descriptive_answers[i]
+                gpt_answer  =  gpt_answers[i]['answer']
+                similarity_score = self._calculate_semantic_similarity(candidate_descriptive_answer, gpt_answer)
+                scores.append(similarity_score)
+                candidate_qna.update_one({"email": self.candidate_email_id},
+                                {"$set": {f"resume_descriptive_qna.{i}.candidate_descriptive_answer": candidate_descriptive_answer,
+                                        f"resume_descriptive_qna.{i}.score": similarity_score}})
+            else:
+                scores.append(0)
+                candidate_qna.update_one({"email": self.candidate_email_id},
+                                {"$set": {f"resume_descriptive_qna.{i}.candidate_descriptive_answer": "No Answer Written",
+                                        f"resume_descriptive_qna.{i}.score": 0}})
+                
+        return np.mean(scores)
+
+    def updateCandidateMCQAnswers(self, mcq_answers:list)->float:
+        ''''
+            Update answer of each MCQ question from the candidate and store the score 
+        '''
+        filter_={"email":self.candidate_email_id}
+        gpt_answers = candidate_qna.find(filter_, {'resume_mcq'}).next()['resume_mcq']
+        scores = []
+        for i in range(len(mcq_answers)):
+            if mcq_answers[i]!="":
+                candidate_mcq_answer = mcq_answers[i]
+                gpt_answer  =  gpt_answers[i]['answer']
+                if gpt_answer==candidate_mcq_answer:
+                    score = 1
+                    scores.append(score)
+                    candidate_qna.update_one({"email": self.candidate_email_id},
+                                    {"$set": {f"resume_mcq.{i}.candidate_mcq_answer": candidate_mcq_answer,
+                                            f"resume_mcq.{i}.score": score}})
+                else:
+                    score = 0
+                    scores.append(score)
+                    candidate_qna.update_one({"email": self.candidate_email_id},
+                                    {"$set": {f"resume_mcq.{i}.candidate_mcq_answer": candidate_mcq_answer,
+                                            f"resume_mcq.{i}.score": score}})
+
+            else:
+                score = 0
+                scores.append(score)
+                candidate_qna.update_one({"email": self.candidate_email_id},
+                                {"$set": {f"resume_mcq.{i}.candidate_mcq_answer": "No Answer Written",
+                                        f"resume_mcq.{i}.score": 0}})
+                
+        return np.mean(scores)
